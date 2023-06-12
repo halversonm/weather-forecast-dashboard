0a3f31b04867e8513a531b069162d2d0f30ae96a
mh <maxhalverson@live.com>
2023-06-12 08:03:42 -0500

    added placeholder html text/layout, linked bootstrap CDNs

---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+ var currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid=36effebef790a646c9774867989342d3';
+ var geocodingUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=36effebef790a646c9774867989342d3';
