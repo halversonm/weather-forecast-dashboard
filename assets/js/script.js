df61b1d8b513d431b03e1622703dccc2fe5a811b
mh <maxhalverson@live.com>
2023-06-12 09:28:02 -0500

    set up DOM manipulation, also set up date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 var searchInput = document.querySelector(".searchInput")
 var searchBtn = document.querySelector(".searchBtn")
+var cityValue = document.querySelector("#cityValue")
+var currentIcon = document.querySelector("#currentIcon")
+var temp = document.querySelector("#temp")
+var windSpeed = document.querySelector("#wind-speed")
+var humidity = document.querySelector("#humidity")
+
 
 // on click on searchBtn we want to grab thae value of what the user typed in, and fetch fetchCurrent()
 searchBtn.addEventListener("click", function () {
@@ -15,5 +21,16 @@ function fetchCurrent(cityName) {
         })
         .then(function (data) {
             console.log(data)
+            var gmt = new Date()
+            var day = gmt.getDate()
+            var month = gmt.getMonth() + 1
+            var year = gmt.getFullYear()
+            var date = `(${month}/${day}/${year})`
+            cityValue.innerHTML = data.name + " " + date
+            currentIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
+            currentIcon.setAttribute("alt", data.weather[0].description)
+            temp.innerHTML = "Temp: " + Math.round(data.main.temp) + " &#176F"
+            windSpeed.innerHTML = "Wind: " + Math.round(data.wind.speed) + "mph"
+            humidity.innerHTML = "Humidity: " + Math.round(data.main.humidity) + "%"
         })
 }
