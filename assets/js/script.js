3aefb04c167b3914d62b0da5abd69385d4c2b8f7
mh <maxhalverson@live.com>
2023-06-12 09:12:19 -0500

    set up function to fetch data, and eventListener to grab search value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
- var currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid=36effebef790a646c9774867989342d3';
- var geocodingUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=36effebef790a646c9774867989342d3';
+var searchInput = document.querySelector(".searchInput")
+var searchBtn = document.querySelector(".searchBtn")
+
+// on click on searchBtn we want to grab thae value of what the user typed in, and fetch fetchCurrent()
+searchBtn.addEventListener("click", function () {
+    var city = searchInput.value
+    fetchCurrent(city)
+})
+
+// function that passes in value of search parameter, and fetchs api based on that query
+function fetchCurrent(cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=36effebef790a646c9774867989342d3`)
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+}
